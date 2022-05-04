30a1573bbdb56f7ef65669a0b4cccbf09851b311
Zuolan <i@zuolan.me>
2022-05-04 16:23:57 +0800

    change: RSS button click to copy

--- components/Hero/Home.js
+++ components/Hero/Home.js
@@ -1,3 +1,4 @@
+import BLOG from '@/blog.config'
 import Link from 'next/link'
 import NotionAvatar from '../NotionAvatar.js'
 import Social from '../Social.js'
@@ -8,13 +9,23 @@ import {
   NewspaperIcon,
   CollectionIcon,
   MailIcon,
-  RssIcon
+  RssIcon,
+  ClipboardCheckIcon
 } from '@heroicons/react/outline'
 
 const Hero = () => {
-  const [showLoading, setShowLoading] = useState(false)
+  const [showCopied, setShowCopied] = useState(false)
   const { locale } = useRouter()
   const t = lang[locale]
+
+  const clickCopy = async () => {
+    setShowCopied(true)
+    navigator.clipboard.writeText(BLOG.link + '/feed')
+    setTimeout(() => {
+      setShowCopied(false)
+    }, 1000)
+  }
+
   return (
     <>
       <div className='container mx-auto flex px-5 py-2 mb-10 md:flex-row flex-col items-center'>
@@ -56,55 +67,34 @@ const Hero = () => {
                 </span>
               </button>
             </Link>
-            {showLoading ? (
+            {showCopied ? (
               <button
                 disabled
-                className='bg-gray-200 dark:bg-gray-600 cursor-not-allowed inline-flex py-3 px-5 rounded-lg items-center'
+                className='bg-gray-200 dark:bg-gray-600 inline-flex py-3 px-5 rounded-lg items-center'
               >
-                <svg
-                  className='animate-spin -ml-1 mr-3 h-5 w-5 text-gray-600 dark:text-day'
-                  xmlns='http://www.w3.org/2000/svg'
-                  fill='none'
-                  viewBox='0 0 24 24'
-                >
-                  <circle
-                    className='opacity-25'
-                    cx='12'
-                    cy='12'
-                    r='10'
-                    stroke='gray'
-                    strokeWidth='4'
-                  ></circle>
-                  <path
-                    className='opacity-75'
-                    fill='currentColor'
-                    d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
-                  ></path>
-                </svg>
+                <ClipboardCheckIcon className='inline-block text-gray-600 dark:text-day h-7 w-7' />
                 <span className='ml-4 flex items-start flex-col leading-none'>
                   <span className='text-xs text-gray-600 dark:text-day mb-1'>
-                    {t.HERO.RSS_BUTTON_DES_LOADING}
+                    {t.HERO.RSS_BUTTON_DES_COPIED}
                   </span>
                   <span className='font-medium'>
-                    {t.HERO.RSS_BUTTON_LOADING}
+                    {t.HERO.RSS_BUTTON_COPIED}
                   </span>
                 </span>
               </button>
             ) : (
-              <Link passHref href='/feed'>
-                <button
-                  onClick={() => setShowLoading(true)}
-                  className='bg-gray-100 dark:bg-gray-700 hover:bg-gray-200 dark:hover:bg-gray-600 inline-flex py-3 px-5 rounded-lg items-center'
-                >
-                  <RssIcon className='inline-block text-gray-600 dark:text-day h-7 w-7' />
-                  <span className='ml-4 flex items-start flex-col leading-none'>
-                    <span className='text-xs text-gray-600 dark:text-day mb-1'>
-                      {t.HERO.RSS_BUTTON_DES}
-                    </span>
-                    <span className='font-medium'>{t.HERO.HOME.RSS_BUTTON}</span>
+              <button
+                onClick={() => clickCopy()}
+                className='bg-gray-100 dark:bg-gray-700 hover:bg-gray-200 dark:hover:bg-gray-600 inline-flex py-3 px-5 rounded-lg items-center'
+              >
+                <RssIcon className='inline-block text-gray-600 dark:text-day h-7 w-7' />
+                <span className='ml-4 flex items-start flex-col leading-none'>
+                  <span className='text-xs text-gray-600 dark:text-day mb-1'>
+                    {t.HERO.RSS_BUTTON_DES}
                   </span>
-                </button>
-              </Link>
+                  <span className='font-medium'>{t.HERO.HOME.RSS_BUTTON}</span>
+                </span>
+              </button>
             )}
           </div>
         </div>
--- components/Hero/Newsletter.js
+++ components/Hero/Newsletter.js
@@ -4,12 +4,21 @@ import Social from '../Social.js'
 import { useState } from 'react'
 import { lang } from '@/lib/lang'
 import { useRouter } from 'next/router'
-import { NewspaperIcon } from '@heroicons/react/outline'
+import { NewspaperIcon, ClipboardCheckIcon } from '@heroicons/react/outline'
 
 const NewsletterHero = () => {
-  const [showLoading, setShowLoading] = useState(false)
+  const [showCopied, setShowCopied] = useState(false)
   const { locale } = useRouter()
   const t = lang[locale]
+
+  const clickCopy = async () => {
+    setShowCopied(true)
+    navigator.clipboard.writeText(BLOG.link + '/newsletter/feed')
+    setTimeout(() => {
+      setShowCopied(false)
+    }, 1000)
+  }
+
   return (
     <>
       <div className='container mx-auto flex px-5 py-2 mb-10 md:flex-row flex-col items-center'>
@@ -48,61 +57,40 @@ const NewsletterHero = () => {
                 </span>
               </button>
             </Link>
-            {showLoading ? (
+            {showCopied ? (
               <button
                 disabled
-                className='bg-gray-200 dark:bg-gray-600 cursor-not-allowed inline-flex py-3 px-5 rounded-lg items-center'
+                className='bg-gray-200 dark:bg-gray-600 inline-flex py-3 px-5 rounded-lg items-center'
               >
-                <svg
-                  className='animate-spin -ml-1 mr-3 h-5 w-5 text-gray-600 dark:text-day'
-                  xmlns='http://www.w3.org/2000/svg'
-                  fill='none'
-                  viewBox='0 0 24 24'
-                >
-                  <circle
-                    className='opacity-25'
-                    cx='12'
-                    cy='12'
-                    r='10'
-                    stroke='gray'
-                    strokeWidth='4'
-                  ></circle>
-                  <path
-                    className='opacity-75'
-                    fill='currentColor'
-                    d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
-                  ></path>
-                </svg>
+                <ClipboardCheckIcon className='inline-block text-gray-600 dark:text-day h-7 w-7' />
                 <span className='ml-4 flex items-start flex-col leading-none'>
                   <span className='text-xs text-gray-600 dark:text-day mb-1'>
-                    {t.HERO.RSS_BUTTON_DES_LOADING}
+                    {t.HERO.RSS_BUTTON_DES_COPIED}
                   </span>
-                  <span className='font-medium'>{t.HERO.RSS_BUTTON_LOADING}</span>
+                  <span className='font-medium'>{t.HERO.RSS_BUTTON_COPIED}</span>
                 </span>
               </button>
             ) : (
-              <Link passHref href='#'>
-                <button
-                  onClick={() => setShowLoading(true)}
-                  className='bg-gray-100 dark:bg-gray-700 hover:bg-gray-200 dark:hover:bg-gray-600 inline-flex py-3 px-5 rounded-lg items-center hover:bg-gray-200'
+              <button
+                onClick={() => clickCopy()}
+                className='bg-gray-100 dark:bg-gray-700 hover:bg-gray-200 dark:hover:bg-gray-600 inline-flex py-3 px-5 rounded-lg items-center hover:bg-gray-200'
+              >
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  fill='currentColor'
+                  className='text-gray-600 dark:text-day w-6 h-6'
+                  viewBox='0 0 24 24'
                 >
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    fill='currentColor'
-                    className='text-gray-600 dark:text-day w-6 h-6'
-                    viewBox='0 0 24 24'
-                  >
-                    <path fill='none' d='M0 0h24v24H0z' />
-                    <path d='M3 17a4 4 0 0 1 4 4H3v-4zm0-7c6.075 0 11 4.925 11 11h-2a9 9 0 0 0-9-9v-2zm0-7c9.941 0 18 8.059 18 18h-2c0-8.837-7.163-16-16-16V3z' />
-                  </svg>
-                  <span className='ml-4 flex items-start flex-col leading-none'>
-                    <span className='text-xs text-gray-600 dark:text-day mb-1'>
-                      {t.HERO.RSS_BUTTON_DES}
-                    </span>
-                    <span className='font-medium'>{t.HERO.NEWSLETTER.RSS_BUTTON}</span>
+                  <path fill='none' d='M0 0h24v24H0z' />
+                  <path d='M3 17a4 4 0 0 1 4 4H3v-4zm0-7c6.075 0 11 4.925 11 11h-2a9 9 0 0 0-9-9v-2zm0-7c9.941 0 18 8.059 18 18h-2c0-8.837-7.163-16-16-16V3z' />
+                </svg>
+                <span className='ml-4 flex items-start flex-col leading-none'>
+                  <span className='text-xs text-gray-600 dark:text-day mb-1'>
+                    {t.HERO.RSS_BUTTON_DES}
                   </span>
-                </button>
-              </Link>
+                  <span className='font-medium'>{t.HERO.NEWSLETTER.RSS_BUTTON}</span>
+                </span>
+              </button>
             )}
           </div>
         </div>
