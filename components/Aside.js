66ce71ac8823ecfdf979972f129f478b78aa921e
ZuoLan <i@zuolan.me>
2022-10-11 13:13:01 +0000

    Fix: lqip and some style bugs.

--- components/Aside.js
+++ components/Aside.js
@@ -23,45 +23,46 @@ const Aside = ({ subPageTitle, frontMatter }) => {
     })
   }, [frontMatter, subPageTitle])
   return (
-    <aside className='hidden sticky md:flex md:flex-col md:items-center md:self-start md:ml-8 md:inset-y-1/2'>
-      <div className='flex flex-col items-center text-center'>
-        <div className='bg-gray-100 dark:bg-gray-700 grid rounded-lg block p-2 gap-y-5 nav'>
-          <button
-            onClick={() => setShowPay((showPay) => !showPay)}
-            className='text-gray-600 dark:text-day hover:text-gray-400 dark:hover:text-gray-400'
-          >
-            <ThumbUpIcon className='w-5 h-5' />
-          </button>
-          {showSubPageTitle && (
-            <Link passHref href={`${BLOG.path}/${frontMatter.slug}`} scroll={false}>
-              <a className='text-gray-600 dark:text-day hover:text-gray-400 dark:hover:text-gray-400'>
-                <ChevronLeftIcon className='w-5 h-5' />
-              </a>
-            </Link>
-          )}
-          {showButton && (
+    <>
+      <aside className='hidden sticky md:flex md:flex-col md:items-center md:self-start md:ml-8 md:inset-y-1/2'>
+        <div className='flex flex-col items-center text-center'>
+          <div className='bg-gray-100 dark:bg-gray-700 grid rounded-lg block p-2 gap-y-5 nav'>
             <button
-              onClick={() =>
-                window.scrollTo({ top: 0, behavior: 'smooth' })
-              }
+              onClick={() => setShowPay((showPay) => !showPay)}
               className='text-gray-600 dark:text-day hover:text-gray-400 dark:hover:text-gray-400'
             >
-              <ArrowUpIcon className='w-5 h-5' />
+              <ThumbUpIcon className='w-5 h-5' />
             </button>
-          )}
+            {showSubPageTitle && (
+              <Link passHref href={`${BLOG.path}/${frontMatter.slug}`} scroll={false}>
+                <a className='text-gray-600 dark:text-day hover:text-gray-400 dark:hover:text-gray-400'>
+                  <ChevronLeftIcon className='w-5 h-5' />
+                </a>
+              </Link>
+            )}
+            {showButton && (
+              <button
+                onClick={() =>
+                  window.scrollTo({ top: 0, behavior: 'smooth' })
+                }
+                className='text-gray-600 dark:text-day hover:text-gray-400 dark:hover:text-gray-400'
+              >
+                <ArrowUpIcon className='w-5 h-5' />
+              </button>
+            )}
+          </div>
         </div>
-      </div>
+      </aside>
       {showPay && <WechatPay />}
-      {/* mobile back to top button */}
       {showButton && (
         <button
           onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
-          className='md:hidden fixed inline-flex z-10 shadow bg-gray-200 hover:bg-gray-300 dark:bg-gray-700 dark:hover:bg-gray-600 p-2 rounded-lg bottom-5 right-5'
+          className='md:hidden fixed inline-flex bottom-5 right-5 p-2 rounded-lg z-10 shadow bg-gray-200 hover:bg-gray-300 dark:bg-gray-700 dark:hover:bg-gray-600'
         >
           <ArrowUpIcon className='text-gray-600 dark:text-day w-5 h-5' />
         </button>
       )}
-    </aside>
+    </>
   )
 }
 
--- components/WechatPay.js
+++ components/WechatPay.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 // export default function WechatPay() {
 const WechatPay = () => {
   return (
-    <div className='fixed inline-flex z-10 shadow-lg bg-gray-100 dark:bg-gray-400 p-5 rounded-full top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
+    <div className='fixed inline-flex shadow-lg bg-gray-100 dark:bg-gray-400 p-5 rounded-full top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
       <Image
         src='/wechat-pay.png'
         alt='WeChat Pay'
