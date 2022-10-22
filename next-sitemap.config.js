6564577c4a1cd0711e128ebd59298bc085efea54
ZuoLan <i@zuolan.me>
2022-10-22 13:08:27 +0000

    Fix: eslint bug

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,6 +3,7 @@ const BLOG = require('./blog.config')
 module.exports = {
   siteUrl: BLOG.link,
   generateRobotsTxt: true,
+  generateIndexSitemap: false,
   sitemapSize: 7000
   // ...other options
   // https://github.com/iamvishnusankar/next-sitemap#configuration-options
