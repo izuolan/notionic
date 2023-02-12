c2875cd9446c7eee5417b46bc4493b31af71e5b0
ZenonMok <124373706+ZenonMok@users.noreply.github.com>
2023-02-12 18:37:30 +0800

    Update Home.js

--- components/Hero/Home.js
+++ components/Hero/Home.js
@@ -1,6 +1,6 @@
 import BLOG from '@/blog.config'
 import Link from 'next/link'
-import Avatar from './NotionAvatar.js'
+import Avatar from './peep (1) (2).svg'
 import Social from '../Common/Social.js'
 import { lang } from '@/lib/lang'
 import { useRouter } from 'next/router'
