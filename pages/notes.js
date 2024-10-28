dc0c1e56cb8f744f614af17f47ba7a172a6eb676
付恒 <2827162018@qq.com>
2024-10-29 01:12:55 +0800

    logo

--- pages/notes.js
+++ pages/notes.js
@@ -1,17 +1,21 @@
-import Container from '@/components/Container'
-import BlogPost from '@/components/BlogPost'
-import NewsletterHero from '@/components/Hero/Newsletter'
-import { getAllPosts, getPostBlocks } from '@/lib/notion'
-import BLOG from '@/blog.config'
+import Container from '@/components/Container' // 导入Container组件
+import BlogPost from '@/components/BlogPost' // 导入BlogPost组件
+import NewsletterHero from '@/components/Hero/Newsletter' // 导入NewsletterHero组件
+import { getAllPosts, getPostBlocks } from '@/lib/notion' // 导入getAllPosts和getPostBlocks函数
+import BLOG from '@/blog.config' // 导入BLOG配置
 
 export async function getStaticProps() {
+  // 获取所有只包含Newsletter的帖子
   const posts = await getAllPosts({ onlyNewsletter: true })
 
+  // 获取所有隐藏的帖子
   const heros = await getAllPosts({ onlyHidden: true })
-  const hero = heros.find((t) => t.slug === 'Notes')
+  // 找到slug为'newsletter'的帖子
+  const hero = heros.find((t) => t.slug === 'newsletter')
 
   let blockMap
   try {
+    // 获取hero的blockMap
     blockMap = await getPostBlocks(hero.id)
   } catch (err) {
     console.error(err)
@@ -27,6 +31,7 @@ export async function getStaticProps() {
   }
 }
 
+// 定义newsletter函数
 const newsletter = ({ posts, blockMap }) => {
   return (
     <Container title={BLOG.newsletter} description={BLOG.description}>
@@ -38,4 +43,4 @@ const newsletter = ({ posts, blockMap }) => {
   )
 }
 
-export default newsletter
+export default newsletter // 导出newsletter函数
