b91a65df3196bcfe26d656f0ad2168700ec1d5d4
付恒 <2827162018@qq.com>
2024-10-29 02:03:49 +0800

    1

--- components/Common/Logo.js
+++ components/Common/Logo.js
@@ -3,10 +3,10 @@ import * as React from 'react'
 
 const Logo = (props) => (
 <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
- width="60.000000pt" height="60.000000pt" viewBox="0 0 60.000000 60.000000"
- preserveAspectRatio="xMidYMid meet">
+width="30.000000pt" height="30.000000pt" viewBox="0 0 30.000000 30.000000"
+preserveAspectRatio="xMidYMid meet">
 
-<g transform="translate(0.000000,60.000000) scale(0.100000,-0.100000)"
+<g transform="translate(0.000000,30.000000) scale(0.100000,-0.100000)"
 fill="#000000" stroke="none">
 <path d="M200 552 c-19 -9 -45 -32 -57 -51 -22 -33 -23 -42 -23 -257 l0 -224
 40 0 40 0 0 115 0 115 100 0 100 0 0 -50 c0 -69 -22 -90 -95 -90 l-55 0 0 -45
@@ -15,6 +15,7 @@ fill="#000000" stroke="none">
 40 l0 40 -62 0 c-40 0 -76 -6 -98 -18z"/>
 </g>
 </svg>
+
 
 
 )
