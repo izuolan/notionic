ab1bfb72bdcde304e3eeab772e34209084381cc7
Zuo Lan <i@zuolan.me>
2025-08-27 00:01:49 +0800

    hero margin style

--- components/Hero/Newsletter.js
+++ components/Hero/Newsletter.js
@@ -25,7 +25,7 @@ const NewsletterHero = ({ blockMap }) => {
       <div className='container mx-auto flex px-5 py-2 mb-10 md:flex-row flex-col items-center'>
         <div className='flex flex-col md:w-4/5 md:items-start mb-6 md:mb-0 md:text-left'>
           <NotionRenderer
-            className='md:ml-0'
+            className='notion-renderer-override'
             blockMap={blockMap}
             frontMatter={{}}
             subPageTitle={null}
