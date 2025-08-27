bd0ee1168a74d2ed59a32b75a38fe8ea711add9a
Zuo Lan <i@zuolan.me>
2025-08-27 17:11:27 +0800

    typo

--- components/Post/SupaComments.js
+++ components/Post/SupaComments.js
@@ -18,8 +18,8 @@ const SupaComments = () => {
   return (
     <>
       {/* SupaComments share existing tailwind css styles, don't need to import an additional css file. */}
-      <div className='hidden mt-10 space-y-4 mt-2 rounded-full w-8 h-8 w-10 h-10' />
-      <div className='hidden animate-pulse space-x-4 flex-1 h-2 w-20 rounded grid-cols-3 gap-4 col-span-1 col-span-2' />
+      <div className='hidden mt-10 space-y-4 rounded-full w-10 h-10' />
+      <div className='hidden animate-pulse space-x-4 flex-1 h-2 w-20 rounded grid-cols-3 gap-4 col-span-2' />
       <div
         id='comments'
         // data-url='localhost:3000'
