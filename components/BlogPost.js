f630c7f3e81723ae21623b01bded7d345033f70a
ZuoLan <i@zuolan.me>
2026-03-21 06:10:29 +0000

    style fixed

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -9,15 +9,15 @@ const BlogPost = ({ post }) => {
       <Link passHref href={`${BLOG.path}/${post.slug}`} scroll={false}>
         <article
           key={post.id}
-          className='group flex flex-col overflow-hidden relative mb-5 md:mb-8 cursor-pointer rounded-xl p-5 h-[120px] md:h-[140px] bg-gray-100 dark:bg-gray-700 hover:bg-gray-200 dark:hover:bg-gray-600'
+          className='group flex flex-col overflow-hidden relative mb-5 md:mb-8 cursor-pointer rounded-xl p-5 md:h-[140px] bg-gray-100 dark:bg-gray-700 hover:bg-gray-200 dark:hover:bg-gray-600'
         >
           <Image
             fill
             alt={`${post.title}`}
             src={post?.page_cover}
-            className='w-full h-full object-cover object-center absolute inset-0 opacity-0 group-hover:opacity-40 group-hover:scale-110 transition-all duration-300 ease-in-out'
+            className='w-full h-full object-cover object-center absolute inset-0 opacity-0 group-hover:opacity-20 dark:group-hover:opacity-40 group-hover:scale-110 transition-all duration-300 ease-in-out'
           />
-          <div className='relative mt-auto'>
+          <div className='relative mt-0'>
             <header className='flex flex-col justify-between md:flex-row md:items-baseline'>
               <h2 className='text-lg md:text-xl font-medium mb-2 text-black dark:text-gray-100 transition-colors duration-300'>{post.title}</h2>
               <span className='font-light shrink-0 text-gray-600 dark:text-gray-400 transition-colors duration-300'>
